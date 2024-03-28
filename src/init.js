a6bd5c18dd49b2dc8458eb4b2792f8e8200d2e51
namrata <rathiNamrata@github.com>
2024-03-28 13:08:15 +0100

    counter

--- src/init.js
+++ src/init.js
@@ -1,1 +1,27 @@
 alert('init.js loaded!');
+
+let counters = 0;
+
+const dom  = { 
+    increment : document.getElementById('increment-btn'),
+    counter : document.querySelector('#counter-value'),
+    decrement : document.getElementById('decrement-btn'),
+    reset : document.getElementById('reset-btn')
+};
+
+const resetBtn = () => {
+    counters = 0;
+    dom.counter.innerHTML = counters;
+}
+
+dom.increment.addEventListener('click',() => {
+    counters++;
+   dom.counter.innerHTML = counters;
+
+});
+ dom.decrement.addEventListener('click',() => {
+    counters--;
+    dom.counter.innerHTML = counters;
+ });
+
+dom.reset.addEventListener('click' ,resetBtn);
